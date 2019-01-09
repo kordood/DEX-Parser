09551c081da0a78002d587635d872011ec0a808e
kordood <gigacms@gmail.com>
2019-01-09 04:38:08 -0800

    make chunk

--- dexparser.h
+++ dexparser.h
@@ -2,36 +2,53 @@
 #include <stdint.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <memory.h>
+#include <sys/mman.h>
 
 typedef struct header_item{
-uint8_t magic[8];
-uint32_t checksum;
-uint8_t signature[20];
-uint32_t file_size;
-uint32_t header_size;
-uint32_t endian_tag;
-uint32_t linksize;
-uint32_t map_off;
-uint32_t string_ids_size;
-uint32_t string_ids_off;
-uint32_t type_ids_size;
-uint32_t type_ids_off;
-uint32_t proto_ids_size;
-uint32_t proto_ids_off;
-uint32_t field_ids_size;
-uint32_t field_ids_off;
-uint32_t method_ids_size;
-uint32_t method_ids_off;
-uint32_t class_defs_size;
-uint32_t class_defs_off;
-uint32_t data_size;
-uint32_t data_off;
+	uint8_t magic[8];
+	uint32_t checksum;
+	uint8_t signature[20];
+	uint32_t file_size;
+	uint32_t header_size;
+	uint32_t endian_tag;
+	uint32_t link_size;
+	uint32_t link_off;
+	uint32_t map_off;
+	uint32_t string_ids_size;
+	uint32_t string_ids_off;
+	uint32_t type_ids_size;
+	uint32_t type_ids_off;
+	uint32_t proto_ids_size;
+	uint32_t proto_ids_off;
+	uint32_t field_ids_size;
+	uint32_t field_ids_off;
+	uint32_t method_ids_size;
+	uint32_t method_ids_off;
+	uint32_t class_defs_size;
+	uint32_t class_defs_off;
+	uint32_t data_size;
+	uint32_t data_off;
 } header_item;
+
+typedef struct pChunk_item{
+	uint32_t *pLink;
+	uint32_t *pString_ids;
+	uint32_t *pMap;
+	uint32_t *pType_ids;
+	uint32_t *pProto_ids;
+	uint32_t *pField_ids;
+	uint32_t *pMethod_ids;
+	uint32_t *pClass_defs;
+	uint32_t *pData;
+} pChunk_item;
 
 void isLittleEndian(uint32_t fp);
 uint32_t getHeaderSize(uint32_t fp);
 void header_slice(uint32_t fp, uint32_t header_size);
+void init_pChunk();
+void *make_chunk(uint32_t fp, void *p, uint32_t offset, uint32_t size);
+void print_header();
 
 _Bool littleEndian = 1;
 header_item *pHeader;
+pChunk_item *pChunk;
--- import_dex.c
+++ import_dex.c
@@ -12,15 +12,18 @@ int main(){
 	else{
 		isLittleEndian(fp);
 		header_size = getHeaderSize(fp);
-		pHeader = (header_item *) malloc(sizeof(header_item));
-		lseek(fp, 0, SEEK_SET);
-		uint8_t header[header_size];
-		read(fp, header, sizeof(uint8_t)*header_size);
+		pHeader = mmap(0, sizeof(header_item), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);		//pHeader = (header_item *) malloc(sizeof(header_item));
+		pChunk = (pChunk_item *) malloc(sizeof(pChunk_item));
 
 		header_slice(fp, header_size);
-
-		for(size_t i = 0; i < header_size; i++){
-			printf("%02x", header[i]);
+		pChunk->pString_ids = (uint32_t *)mmap(0, sizeof(uint32_t) * pHeader->string_ids_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);		//pChunk->pString_ids = (uint32_t *) malloc(sizeof(uint32_t) * pHeader->string_ids_size);
+		printf("size and offset is %d\n%d\n", pHeader->string_ids_size, pHeader->string_ids_off);
+		pChunk->pString_ids = (uint32_t *)make_chunk(fp, (uint32_t *)pChunk->pString_ids, pHeader->string_ids_off, sizeof(uint32_t) * pHeader->string_ids_size);
+		for(size_t i = 0; i < pHeader->string_ids_size; i++){
+			printf("%08x ", pChunk->pString_ids[i]);
+			if(((i+1) % 4) == 0 && i > 0){
+				printf("\n");
+			}
 		}
 	}
 
@@ -51,18 +54,75 @@ uint32_t getHeaderSize(uint32_t fp){	// default: LittleEndian, have to make proc
 	lseek(fp, 36, SEEK_SET);
 	read(fp, &header_size, sizeof(uint32_t));
 
-	printf("%d\n", header_size);
+	printf("%d\t\t\t\t\t\t\t      header size(int)\n", header_size);
 
 	return header_size;
 }
 
 void header_slice(uint32_t fp, uint32_t header_size){
+	lseek(fp, 0, SEEK_SET);
 	read(fp, pHeader, sizeof(uint8_t)*header_size);
+	print_header();
+}
 
+void *make_chunk(uint32_t fp,void *p, uint32_t offset, uint32_t size){
+	lseek(fp, offset, SEEK_SET);
+	read(fp, p, size);
+	
+	return p;
+}
+
+void print_header(){
+	uint8_t *tmp = "";
 	for(size_t i = 0; i < 8; i++){
 		printf("%02x", pHeader->magic[i]);
 	}
-	printf("magic\n");
-	
+	printf("\t\t\t\t\t      magic\n");
 
+	printf("%08x %-52s checksum\n", pHeader->checksum, tmp);
+
+	for(size_t i = 0; i < 20; i++){
+		printf("%02x", pHeader->signature[i]);
+	}
+	printf("\t\t      signature\n");
+	
+	printf("%08x %-52s file_size\n", pHeader->file_size, tmp);
+
+	printf("%08x %-52s header_size\n", pHeader->header_size, tmp);
+
+	printf("%08x %-52s endian_tag\n", pHeader->endian_tag, tmp);
+
+	printf("%08x %-52s link_size\n", pHeader->link_size, tmp);
+
+	printf("%08x %-52s link_off\n", pHeader->link_off, tmp);
+
+	printf("%08x %-52s map_off\n", pHeader->map_off, tmp);
+
+	printf("%08x %-52s string_ids_size\n", pHeader->string_ids_size, tmp);
+
+	printf("%08x %-52s string_ids_off\n", pHeader->string_ids_off, tmp);
+
+	printf("%08x %-52s type_ids_size\n", pHeader->type_ids_size, tmp);
+
+	printf("%08x %-52s type_ids_off\n", pHeader->type_ids_off, tmp);
+
+	printf("%08x %-52s proto_ids_size\n", pHeader->proto_ids_size, tmp);
+
+	printf("%08x %-52s proto_ids_off\n", pHeader->proto_ids_off, tmp);
+
+	printf("%08x %-52s field_ids_size\n", pHeader->field_ids_size, tmp);
+
+	printf("%08x %-52s field_ids_off\n", pHeader->field_ids_off, tmp);
+
+	printf("%08x %-52s method_ids_size\n", pHeader->method_ids_size, tmp);
+
+	printf("%08x %-52s method_ids_off\n", pHeader->method_ids_off, tmp);
+
+	printf("%08x %-52s class_defs_size\n", pHeader->class_defs_size, tmp);
+
+	printf("%08x %-52s class_defs_off\n", pHeader->class_defs_off, tmp);
+
+	printf("%08x %-52s data_size\n", pHeader->data_size, tmp);
+
+	printf("%08x %-52s data_off\n", pHeader->data_off, tmp);
 }
