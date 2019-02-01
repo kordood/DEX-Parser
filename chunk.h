a7af0aa018c3ff1e1b6382d041c582f6a67d1122
kordood <gigacms@gmail.com>
2019-02-01 03:37:43 -0800

    typeList into FileLayout

--- chunk.h
+++ chunk.h
@@ -1,8 +1,7 @@
 #include "dexparser.h"
 
+void make_map_list(uint32_t fp, map_list *mapList);
+void delete_map_list(map_list *mapList);
 void make_chunk(uint32_t fp, file_layout **pFileLayout);
 void read_chunk(uint32_t fp, void *pItem, uint32_t offset, uint32_t size);
 void delete_chunk(file_layout **pFileLayout);
-void make_map_list(uint32_t fp, map_list *mapList);
-void delete_map_list(map_list *mapList);
-//void print_chunk(uint32_t *pItem, uint32_t type_size, uint32_t size);
--- print.h
+++ print.h
@@ -1,15 +1,15 @@
 #include "dexparser.h"
 
-void print_all(uint32_t fp, file_layout **pChunk, string_data_item *pString_list, type_list *paramTypeList, map_list *mapList);
+void print_all(uint32_t fp, file_layout **pFileLayout, string_data_item *pString_list, type_list *typeList, type_list *paramTypeList, map_list *mapList);
 void print_string_item(string_data_item *pString_list, uint32_t size);
-void print_type_list(string_data_item *pString_list);
+void print_type_list(string_data_item *pString_list, type_list *typeList);
 void print_link(uint32_t *pItem, uint32_t size);
 void print_string_ids(uint32_t *pItem, uint32_t size);
 void print_type_ids(uint32_t *pItem, string_data_item *pString_list, uint32_t size);
-void print_proto_ids(proto_id_item *pItem, string_data_item *pString_list, uint32_t size);
-void print_proto_type(type_list *pTypeList, string_data_item *pString_list, uint32_t size);
-void print_field_ids(field_id_item *pItem, string_data_item *pString_list, uint32_t size);
-void print_method_ids(method_id_item *pItem, string_data_item *pString_list, uint32_t size);
-void print_class_defs(class_def_item *pItem, string_data_item *pString_list, uint32_t size);
+void print_proto_ids(proto_id_item *pItem, string_data_item *pString_list, type_list *typeList, uint32_t size);
+void print_proto_type(type_list *pTypeList, string_data_item *pString_list,type_list *typeList, uint32_t size);
+void print_field_ids(field_id_item *pItem, string_data_item *pString_list, type_list *typeList, uint32_t size);
+void print_method_ids(method_id_item *pItem, string_data_item *pString_list, type_list *typeList, uint32_t size);
+void print_class_defs(class_def_item *pItem, string_data_item *pString_list, type_list *typeList, uint32_t size);
 void print_access_flag(enum access_flags aces_flag);
 void print_map_list(map_list *mapList);
